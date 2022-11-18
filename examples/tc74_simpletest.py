05e2b6aac574d2ce2ac2468c5d212836592bf6f7
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:30 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/tc74_simpletest.py
+++ examples/tc74_simpletest.py
@@ -5,7 +5,8 @@ import time
 import board
 import adafruit_tc74
 
-i2c = board.I2C()
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 tc = adafruit_tc74.TC74(i2c)
 
 while True:
